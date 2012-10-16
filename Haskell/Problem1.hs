1d6820661f3811ec3e6517aef8c49d86288ca57c
Tobias Savinainen <tcs1000@hotmail.com>
2012-10-16 22:56:56 +0200

    Added description

--- Haskell/Problem1.hs
+++ Haskell/Problem1.hs
@@ -1,5 +1,6 @@
 ------------------------------------------------------------
 -- Problem 1
+--
 -- If we list all the natural numbers below 10 that are 
 -- multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
 -- these multiples is 23.
@@ -8,7 +9,7 @@
 ------------------------------------------------------------
 
 ------------------------------------------------------------
--- Current version
+-- Enter a list as an argument
 ------------------------------------------------------------
 problem1 :: [Integer] -> Int
 problem1 [] = 0
